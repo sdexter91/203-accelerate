80f13cd4643aef4ebcb407d05b1172f5b57d8b43
Sam Dexter <sdexter91@gmail.com>
2020-06-17 17:46:51 +0200

    404 page

---
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying the 404 page
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content sidebar">
+		<div class="main-content" role="main">
+			<div class="image-container">
+				
+			</div>
+			<div class="not-found-text">
+				<h1>Whoops, Took a Wrong Turn...</h1>
+				<p>Sorry, this page no longer exists, never existed or has been moved. We feel like complete jerks for totally misleading you.</p>
+				<p>Feeel free to take a look around our <a href="http://localhost:8888/203-accelerate/blog/">blog</a> or some of our featured <a href="http://localhost:8888/203-accelerate/case-studies/">work</a>.</p>
+			</div>
+		</div><!-- .main-content -->
+
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
